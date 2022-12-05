f7674fe689577f90e80493e5d5168cff4aaf4a16
Yasser <blackbird@MacBook-Pro-de-Lanjri.local>
2022-12-05 01:54:13 +0100

    correction error-alert file

--- components/ui/error-alert.tsx
+++ components/ui/error-alert.tsx
@@ -1,6 +1,7 @@
+import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from 'react';
 import classes from './error-alert.module.css';
 
-function ErrorAlert(props) {
+function ErrorAlert(props: { children: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }) {
   return <div className={classes.alert}>{props.children}</div>;
 }
 
