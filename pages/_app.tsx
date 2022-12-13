1745991a3172b850b9a888fd84af2e558768d628
Yasser <blackbird@MacBook-Pro-de-Lanjri.local>
2022-12-14 00:48:56 +0100

    correcting types errors on useContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <NotificationContextProvider>
             <Layout>
                 <Component {...pageProps} />
-                
             </Layout>
         </NotificationContextProvider>
     );
